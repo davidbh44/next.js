4b2ff441f896008539e2d9eff2523fc9f1fc3e4c
Luke Sandberg <lukeisandberg@gmail.com>
2025-06-11 14:32:27 -0700

    simplify construction with a separate constructor

--- crates/next-api/src/project.rs
+++ crates/next-api/src/project.rs
@@ -1319,7 +1319,7 @@ impl Project {
                 self.next_config(),
                 self.execution_context(),
             ),
-            LayerName::new(rcstr!("middleware-edge"), None),
+            LayerName::new(rcstr!("middleware-edge")),
         )))
     }
 
@@ -1373,7 +1373,7 @@ impl Project {
                 self.next_config(),
                 self.execution_context(),
             ),
-            LayerName::new(rcstr!("middleware"), None),
+            LayerName::new(rcstr!("middleware")),
         )))
     }
 
@@ -1485,7 +1485,7 @@ impl Project {
                 self.next_config(),
                 self.execution_context(),
             ),
-            LayerName::new(rcstr!("instrumentation"), None),
+            LayerName::new(rcstr!("instrumentation")),
         )))
     }
 
@@ -1540,7 +1540,7 @@ impl Project {
                 self.next_config(),
                 self.execution_context(),
             ),
-            LayerName::new(rcstr!("instrumentation-edge"), None),
+            LayerName::new(rcstr!("instrumentation-edge")),
         )))
     }
 
