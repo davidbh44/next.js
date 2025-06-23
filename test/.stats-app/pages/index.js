d7f66868e8aa01830f8325e993bab5cb0185307c
davidbh44 <davimarkett@gmail.com>
2025-06-23 18:04:50 +0100

    Update index.js

--- test/.stats-app/pages/index.js
+++ test/.stats-app/pages/index.js
@@ -1,11 +1,9 @@
-const Page = () => 'Hello world 👋'
-
-// we add getServerSideProps to prevent static optimization
-// to allow us to compare server-side changes
-export const getServerSideProps = () => {
-  return {
-    props: {},
-  }
+export default function Home() {
+  return (
+    <div className="text-center mt-10">
+      <h1 className="text-4xl font-bold">Spin & Win - Europe's Mystery Roulette</h1>
+      <p className="mt-4">Try your luck and win amazing prizes!</p>
+    </div>
+  )
 }
 
-export default Page
